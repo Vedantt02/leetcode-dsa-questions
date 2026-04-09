9845db14033846fe5460a7e8ded5df31829cdb96
Vedant Singh Parihar <vedantparihar02@gmail.com>
2026-04-09 23:54:10 +0530

    Time: 473 ms (93.6%), Space: 22.2 MB (62.93%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,7 +4,10 @@ class Solution:
         result = []
 
         for i in range(len(nums)):
-            # Skip duplicate values for i
+            # 🔥 Optimization: break early
+            if nums[i] > 0:
+                break
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
@@ -21,11 +24,8 @@ class Solution:
                 else:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    # Skip duplicates for left
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
-
-                    # Skip duplicates for right
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
 
