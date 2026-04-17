a146fcc51d36ebd32aafee5a862111bc9ce58bd9
Vedant Singh Parihar <vedantparihar02@gmail.com>
2026-04-17 21:10:42 +0530

    Time: 3 ms (88.01%), Space: 86.5 MB (64.91%) - LeetHub

---
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+        int leftMax = nums[0];
+        int currMax = nums[0];
+        int partition = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currMax = Math.max(currMax, nums[i]);
+
+            if (nums[i] < leftMax) {
+                partition = i;
+                leftMax = currMax;
+            }
+        }
+        return partition + 1;
+    }
+}
